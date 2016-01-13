7ed910e93013d92939d968cc803e3a79ad8aa908
Jan Dalheimer <jan@dalheimer.de>
2016-01-13 22:49:56 +0100

    NOISSUE Add license

--- cmake/FindLibGit2.cmake
+++ cmake/FindLibGit2.cmake
@@ -1,3 +1,8 @@
+# Copyright (C) 2000 Jan Dalheimer <jan@dalheimer.de>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+
 find_package(PkgConfig QUIET)
 pkg_search_module(PC_LIBGIT2 libgit2)
 
